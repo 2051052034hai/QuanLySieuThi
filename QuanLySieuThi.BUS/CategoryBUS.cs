b66381929722ceb35668bb7f14f96660100b0cf7
2051052034hai <2051052034hai@ou.edu.vn>
2023-11-08 19:40:10 +0700

    update master

--- QuanLySieuThi.BUS/CategoryBUS.cs
+++ QuanLySieuThi.BUS/CategoryBUS.cs
@@ -62,5 +62,6 @@ namespace QuanLySieuThi.BUS
                 return false;
             }
         }
+
     }
 }
--- QuanLySieuThi/Controllers/HomeController.cs
+++ QuanLySieuThi/Controllers/HomeController.cs
@@ -14,31 +14,38 @@ namespace QuanLySieuThi.Controllers
     [CommonAttributeFilter]
     public class HomeController : Controller
     {
-        public ActionResult Index([Bind(Prefix = "search")] string searchKw, [Bind(Prefix = "categoryId")] string categoryId)
+        public ActionResult Index()
         {
             ProductBUS productBUS = new ProductBUS();
-            List<Product> products = null;
-            if (!searchKw.IsNullOrWhiteSpace())
-                products = productBUS.GetProducts(searchKw);
-            else
-                products = productBUS.GetProducts();
-            if (categoryId != null)
-            {
-                List<Product> products1 = (List<Product>)products.Where(p => p.CateID == int.Parse(categoryId)).ToList();
-                products = products1;
-            }
+            List<Product> products = productBUS.GetProducts();
             ViewBag.Products = products;
 
             return View();
         }
 
-        //public ActionResult Product(string id)
-        //{
-        //    ProductBUS productBUS = new ProductBUS();
-        //    Product product = productBUS.GetProduct(id);
-        //    ViewBag.Product = product;
-        //    return View();
-        //}
+        public ActionResult CategoryProduct([Bind(Prefix = "search")] string searchKw, [Bind(Prefix = "categoryId")] string categoryId)
+        {
+            ProductBUS productBUS = new ProductBUS();
+            CategoryBUS categoryBUS = new CategoryBUS();
+            Category cate = new Category();
+            List<Product> products = null;
+
+            if (!searchKw.IsNullOrWhiteSpace())
+                products = productBUS.GetProducts(searchKw);
+            else
+                products = productBUS.GetProducts();
+
+            if (categoryId != null)
+            {
+                List<Product> products1 = (List<Product>)products.Where(p => p.CateID == int.Parse(categoryId)).ToList();
+                products = products1;
+                int categoryIdInt = Convert.ToInt32(categoryId);
+                cate = categoryBUS.GetCategoryById(categoryIdInt);
+            }
+            ViewBag.Products = products;
+            ViewBag.category = cate;
+            return View();
+        }
 
         public ActionResult Category(string id)
         {
